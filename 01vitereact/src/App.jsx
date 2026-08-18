d6bdfe1e2df9f421f8cf579cd6dd48f9e474b71c
Faizan Hussain <faizanhussain0601@gmail.com>
2026-08-18 18:11:12 +0530

    React basic and vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,13 @@
+import Chai from "./chai"
+function App() {
+
+  return (
+    <>
+    <Chai/>
+    <h1>Chai aur React</h1>
+    <p>Test Para</p>
+    </>
+  )
+}
+
+export default App
---
+++ 01vitereact/src/chai.jsx
@@ -0,0 +1,7 @@
+function Chai(){
+    return (
+        <h3>I am Ready</h3>
+    )
+}
+
+export default Chai
